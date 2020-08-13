2695191827f190a15ce0fc3115c3877f75806eda
PixelQuasar <48384867+PixelQuasar@users.noreply.github.com>
2020-08-14 01:59:36 +0200

    new build

--- time-contrast/sw.js
+++ time-contrast/sw.js
@@ -18,8 +18,8 @@
 'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v27';
-const DATA_CACHE_NAME = 'data-cache-v27';
+const CACHE_NAME = 'static-cache-v29';
+const DATA_CACHE_NAME = 'data-cache-v29';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
